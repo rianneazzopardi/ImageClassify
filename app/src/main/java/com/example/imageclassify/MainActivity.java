527ec0f1fa913c6c98f08b0bd5ba7f138fd8b451
Rianne Azzopardi <rianneazzopardi@gmail.com>
2023-06-15 11:39:48 +0200

    new image classification app

---
+++ app/src/main/java/com/example/imageclassify/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.imageclassify;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button selectBtn, predictBtn, captureBtn;
+    TextView result;
+    Bitmap bitmap;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        selectBtn = findViewById(R.id.selectBtn);
+        predictBtn = findViewById(R.id.predictBtn);
+        captureBtn = findViewById(R.id.captureBtn);
+        result = findViewById(R.id.result);
+        imageView = findViewById(R.id.imageView);
+
+        selectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 10);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==10){
+            if(data!=null){
+                Uri uri = data.getData();
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                    imageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
